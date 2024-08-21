b7c4df101eab515083bd396cb0e53fa31f2b2162
ReBy298 <rebeca.mendoza@encora.com>
2024-08-21 13:53:15 -0600

    First commit

---
+++ Back-End/flight-search/src/test/java/com/assigment/flightsearch/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.flightsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
